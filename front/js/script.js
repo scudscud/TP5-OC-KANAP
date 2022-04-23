459b808dff987ebf6d27c9f7173c464d1db4aa94
scud <guillaume.scudetti@gmail.com>
2022-04-23 12:09:20 +0200

    API product+detail

--- front/js/script.js
+++ front/js/script.js
@@ -1,3 +1,4 @@
+//* Recuperer les informations du produit 
 function getproduct() {
   fetch("http://localhost:3000/api/products")
     .then(function (res) {
@@ -8,9 +9,27 @@ function getproduct() {
     .then(function (products) {
       for (let index = 0; index < products.length; index++) {
         const element = products[index];
-        console.log("price ", element.price);
+        console.log(element.colors[1]);
       }
     });
 }
 
 getproduct();
+
+//* recuperer une information précise 
+function getproductdetail() {
+  fetch("http://localhost:3000/api/products")
+    .then(function (res) {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then(function (products) {
+      for (let index = 0; index < products.length; index++) {
+        const element = products[index];
+        console.log(products[0].colors[1]);
+      }
+    });
+}
+
+getproductdetail()
