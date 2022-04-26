9148b6fec9cdc603a879a4ba7b8819808e976a9d
scud <guillaume.scudetti@gmail.com>
2022-04-26 21:59:10 +0200

    save

--- front/js/product.js
+++ front/js/product.js
@@ -1,44 +1,53 @@
 // const titleprod = document.getElementById("title");
-// const priceprod = document.getElementById("price");
+// let priceprod = document.getElementById("price");
 // const descriptionprod = document.getElementById("description");
 // let choicecolor = document.getElementById("colors[i]");
-
-
-
 
 //* insertion des détails de la description produit sur la page produit *\\
 //* variable de récupération de l'ID produit *\\
 let IdProduct = window.location.search.split("?id=").join("");
-
 let itemImg = document.querySelector(".item__img");
 
 //*  requete pour obtenir les détails de la descritpion produit *\\
 const getproductdetail = async () => {
-    productdetail = await fetch(`http://localhost:3000/api/products/${IdProduct}`)
-        .then((res) => {
-            if (res.ok) {
-                return res.json();
-            }
-        })
-        .then((detail) => {
-            document.title = `${detail.name}`;
-            itemImg.innerHTML = `<img src="${detail.imageUrl}" alt="${detail.altTxt}">`;
-            title.textContent = `${detail.name}`;
-            price.textContent = `${detail.price}`;
-            description.textContent = `${detail.description}`;
-            detail.colors.forEach((n,i) => {
-                colors.innerHTML += `<option value=${detail.colors[i]}>${detail.colors[i]}</option>`
-                console.log(detail.colors);
-             });
-            
-        });
+   await fetch(`http://localhost:3000/api/products/${IdProduct}`)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then((product) => {
+     
+      document.title = `${product.name}`;
+      itemImg.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
+      title.textContent = `${product.name}`;
+      price.textContent = `${product.price}`;
+      description.textContent = `${product.description}`;
+      product.colors.forEach((n, i) => {
+        colors.innerHTML+= `<option value=${product.colors[i]}>${product.colors[i]}</option>`;
+        console.log(IdProduct);
+        console.log(product.colors[i]);
+        // console.log(`${product.price}`);
+      });
+       InputEvent
+        console.log(quantity.value);
+
+
+      
+       
+    //   basket = (IdProduct,product.colors,quantity);
+   
+      
+    });
 };
 
-const details = getproductdetail();
 
+
+
+const product = getproductdetail();
 //********** même resultat boucle for  ************//
 // for ( let i = 0; i < detail.colors.length; i++ ) {
-            //    colors.innerHTML += `<option value="${detail.colors[i]}">${detail.colors[i]}</option>
-            //     `
-            //     console.log(detail.colors[i]);
-            // }
+//    colors.innerHTML += `<option value="${detail.colors[i]}">${detail.colors[i]}</option>
+//     `
+//     console.log(detail.colors[i]);
+// }
