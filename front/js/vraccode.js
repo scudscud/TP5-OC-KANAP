8c9b43bb09a7586b6dcd47b89b2872442be5153c
scud <guillaume.scudetti@gmail.com>
2022-05-10 19:06:25 +0200

    commit readme

--- front/js/vraccode.js
+++ front/js/vraccode.js
@@ -31,100 +31,100 @@
 // };
 // SpawnItem() 
 
-//* boucle for each ok bug apparartion 1 er item dans panier **///
-const addCart = () => {
-  let cart = getCart();
+// //* boucle for each ok bug apparartion 1 er item dans panier **///
+// const addCart = () => {
+//   let cart = getCart();
  
-  cart.forEach((o,i)=>{
- let find = cart.find(c => cart[i][0].id == order[0].id && cart[i][0].color == order[0].color)
- console.log(cart);
-  //   console.log(find);
-  // console.log(cart[0][0].color);
-  // console.log(order[0].color);
-  // console.log(cart[0][0].id);
-  //   console.log(order[0].id);
-   if( find != undefined) {
-    // console.log(cart[0][0].quantity);
-    //   console.log(order[0].quantity);
-    //   console.log("test");
-      cart[i][0].quantity = order[0].quantity
+//   cart.forEach((o,i)=>{
+//  let find = cart.find(c => cart[i][0].id == order[0].id && cart[i][0].color == order[0].color)
+//  console.log(cart);
+//   //   console.log(find);
+//   // console.log(cart[0][0].color);
+//   // console.log(order[0].color);
+//   // console.log(cart[0][0].id);
+//   //   console.log(order[0].id);
+//    if( find != undefined) {
+//     // console.log(cart[0][0].quantity);
+//     //   console.log(order[0].quantity);
+//     //   console.log("test");
+//       cart[i][0].quantity = order[0].quantity
       
 
-   } else { 
+//    } else { 
      
     
-    cart.push(order)
-    saveCart(cart)
+//     cart.push(order)
+//     saveCart(cart)
    
-   }})
-   saveCart(cart)
+//    }})
+//    saveCart(cart)
 
 
-}
+// }
 
-//* ajout d'item local storage sans bug **//
+// //* ajout d'item local storage sans bug **//
 
-const addCart = () => {
-  let cart = getCart();
+// const addCart = () => {
+//   let cart = getCart();
  
-let find = cart.find(c => cart[0][0].id == order[0].id && cart[0][0].color == order[0].color)
+// let find = cart.find(c => cart[0][0].id == order[0].id && cart[0][0].color == order[0].color)
 
-  //   console.log(find);
-  // console.log(cart[0][0].color);
-  // console.log(order[0].color);
-  // console.log(cart[0][0].id);
-  //   console.log(order[0].id);
-   if( find != undefined) {
-    // console.log(cart[0][0].quantity);
-    //   console.log(order[0].quantity);
-    //   console.log("test");
-      cart[0][0].quantity = order[0].quantity
-
-
-   } else { 
+//   //   console.log(find);
+//   // console.log(cart[0][0].color);
+//   // console.log(order[0].color);
+//   // console.log(cart[0][0].id);
+//   //   console.log(order[0].id);
+//    if( find != undefined) {
+//     // console.log(cart[0][0].quantity);
+//     //   console.log(order[0].quantity);
+//     //   console.log("test");
+//       cart[0][0].quantity = order[0].quantity
 
 
-    cart.push(order)
-    saveCart(cart)
-
-   }
-   saveCart(cart)
+//    } else { 
 
 
+//     cart.push(order)
+//     saveCart(cart)
+
+//    }
+//    saveCart(cart)
 
 
 
-}
-//* boucle spawn **\\\
 
-const addCart = () => { 
+
+// }
+// //* boucle spawn **\\\
+
+// const addCart = () => { 
           
-  let cart = getCart(); 
-  // console.log(cart);
- if(localStorage.getItem("order") === null )
-  {
-    saveCart([order]) 
+//   let cart = getCart(); 
+//   // console.log(cart);
+//  if(localStorage.getItem("order") === null )
+//   {
+//     saveCart([order]) 
 
-  }
-  else if (localStorage.getItem("order") !== null ) {
+//   }
+//   else if (localStorage.getItem("order") !== null ) {
   
  
-    cart.forEach((o,i,u)=>{
-     let find = cart.find(c => cart[i][0].id == order[0].id && cart[i][0].color == order[0].color)
-     if( find != undefined) {           
-      cart[i][0].quantity = order[0].quantity
-      // saveCart(cart) 
-    }
+//     cart.forEach((o,i,u)=>{
+//      let find = cart.find(c => cart[i][0].id == order[0].id && cart[i][0].color == order[0].color)
+//      if( find != undefined) {           
+//       cart[i][0].quantity = order[0].quantity
+//       // saveCart(cart) 
+//     }
       
     
       
       
-      else{
+//       else{
         
-        cart.push(order)
-        saveCart(cart)
+//         cart.push(order)
+//         saveCart(cart)
 
 
-      }
-    }) 
-  }
+//       }
+//     }) 
+//   }}
